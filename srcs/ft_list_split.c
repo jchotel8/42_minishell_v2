706d2cbeff0b5c206bfa7da56916b981a12d2863
jchotel8 <julchotel@gmail.com>
2020-06-12 14:56:10 +0200

    merging

--- srcs/ft_list_split.c
+++ srcs/ft_list_split.c
@@ -1,10 +1,51 @@
 #include "../includes/minishell.h"
 
-t_list *ft_list_split(char *s)
+t_list *ft_list_split(char *s, char c)
 {
     t_list *lst;
+    char *ptr;
 
+    ptr = 0;
+    if (!s )
+    
+    while (*s)
+    {
+        if (*s == c)
+        {
+            if (ptr != s)
+                ft_listadd_back(lst, ft_lstnew(ft_substr(ptr, 0, s - ptr)));
+            ptr = (char *)s + 1;
+        }
+        s++;
+    }
+    if (ptr != s)
+        ft_listadd_back(lst, ft_lstnew(ft_substr(ptr, 0, s - ptr)));
     //lst = ft_listadd_front(ft_lstnew(strdup)
     return (*lst);
 }
 
+void	ft_list_print(t_list *ptr, int flag)
+{
+	int	i = 0;
+
+	if (ptr)
+	{
+		flag == 1 ? printf("[%d] : %s\n", i, ptr->content) :
+		printf("%s\n", ptr->content);
+		while(ptr->next)
+		{
+			ptr = ptr->next;
+			i++;
+			flag == 1 ? printf("[%d] : %s\n", i, ptr->content) :
+			printf("%s\n", ptr->content);
+		}
+	}
+}
+
+int main(int ac, char **av)
+{
+    t_list *l;
+
+    l = ft_list_split("bonjour je suis juliette");
+    
+}
--- srcs/main.c
+++ srcs/main.c
@@ -1,12 +1,12 @@
 #include "../includes/minishell.h"
 
-int main()
-{
-    char *stock;
-    ft_printf("MINISHELL : ");
-    while (get_next_line(0, &stock)) 
-    {
-        ft_printf("%s\n", stock);
-        ft_printf("MINISHELL :");
-    }
-}
+// int main()
+// {
+//     char *stock;
+//     ft_printf("MINISHELL : ");
+//     while (get_next_line(0, &stock)) 
+//     {
+//         ft_printf("%s\n", stock);
+//         ft_printf("MINISHELL :");
+//     }
+// }

970412d8669210c39711a9b8848cf76376ff3403
jchotel8 <julchotel@gmail.com>
2020-07-01 15:47:12 +0200

    Error redirected on error

---
+++ srcs/ft_pipes.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pipes.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchotel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/06 10:11:09 by jchotel           #+#    #+#             */
+/*   Updated: 2020/03/03 12:27:12 by jchotel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	init_pipes(int nb_pipes, int *pipes)
+{
+	int i;
+
+	i = 0;
+	while ((2 * i) < nb_pipes)
+		pipe(pipes + (2 * i++));
+}
+
+void	close_pipes(int nb_pipes, int *pipes)
+{
+	while (nb_pipes--)
+		close(pipes[nb_pipes]);
+}
+
+void	wait_pipes(int nb_cmd, pid_t *pid, int *ret)
+{
+	int i;
+
+	i = 0;
+	while (i < nb_cmd)
+		waitpid(pid[i++], ret, 0);
+}
+
+void	do_dup(int j, int nb_cmd, int *pipes, t_pipe *p)
+{
+	int	fd;
+	t_list *tmp;
+	t_list *tmpt;
+
+	if (j > 0)
+		dup2(pipes[j * 2 - 2], 0);
+	tmp = p->redirg;
+	while (tmp)
+	{
+		if ((fd = open(tmp->content, O_RDONLY)) < 0)
+			return ;
+		dup2(fd, 0);
+		tmp = tmp->next;
+	}
+	tmpt = p->typed;
+	if (j < nb_cmd - 1 || p->redird)
+	{
+		tmp = p->redird;
+		while (tmp)
+		{
+			if (!ft_strcmp(tmpt->content, "1"))
+				pipes[j * 2 + 1] = open(tmp->content, O_WRONLY | O_CREAT | O_TRUNC | O_RDONLY, 0644);
+			else if (!ft_strcmp(tmpt->content, "2"))
+				pipes[j * 2 + 1] = open(tmp->content, O_WRONLY | O_CREAT | O_APPEND | O_RDONLY, 0644);
+			tmp = tmp->next;
+			tmpt = tmpt->next;
+		}
+		dup2(pipes[j * 2 + 1], 1);
+	}
+}
+
+void	free_pipe(t_pipe *p)
+{
+	ft_freearray(p->cmd);
+	ft_lstclear(&p->redirg, *free);
+	ft_lstclear(&p->redird, *free);
+	ft_lstclear(&p->typeg, *free);
+	ft_lstclear(&p->typed, *free);
+}
+
+void	do_pipe(t_list *line, int nb_cmd, int *ret, t_list **env)
+{
+	pid_t	pid[nb_cmd];
+	int		pipes[nb_cmd * 2 - 2];
+	int		j;
+	t_pipe	p;
+	int		flag;
+
+	flag = 0;
+	j = -1;
+	init_pipes(nb_cmd * 2 - 2, pipes);
+	while (++j < nb_cmd)
+	{
+		if (parse_redir(line->content, &p, *env))
+			*ret = 1;
+		else
+		{
+			if (p.cmd && !ft_strcmp(p.cmd[0], "exit"))
+				flag = 1;
+			else if (p.cmd && (nb_cmd > 1 || p.redird != NULL || p.redirg != NULL) && !(pid[j] = fork()))
+			{
+				do_dup(j, nb_cmd, pipes, &p);
+				close_pipes(nb_cmd * 2 - 2, pipes);
+				*ret = ft_exec(p.cmd, env);
+				free_pipe(&p);
+				if (*ret <= 0) //renvoi 0 ou 8 si un des bins (S/F) sinon : NA if success, -1 if fail, 127 if not found, 1 if ?
+					exit(0); //il faut exit seulement dans le cas ou execve fail et si une de mes commandes fails
+				if (*ret == 8)
+					exit(1);
+				if (*ret == 127)
+					exit(3);
+			}
+			else if (p.cmd && nb_cmd == 1 && p.redird == NULL && p.redirg == NULL)
+			{
+				if ((*ret = ft_exec2(p.cmd, env)) == 127)
+					exit(3);
+			}
+		}
+		free_pipe(&p);
+	 	line = line->next;
+	}
+	close_pipes(nb_cmd * 2 - 2, pipes);
+	wait_pipes(nb_cmd, pid, ret);
+	if (flag == 1)
+		ft_exit(p.cmd, *env);
+}
---
+++ srcs/main.c
@@ -0,0 +1,154 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c	                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchotel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/06 10:11:09 by jchotel           #+#    #+#             */
+/*   Updated: 2020/03/03 12:27:12 by jchotel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+/*
+** THINGS TO DO :
+** - ft_exit : free when fail
+** - ctrl-C, ctrl-D, ctrl-/
+** - echo bonjour > "newline error"
+*/
+
+int		rep;
+
+void    ft_prompt(void)
+{
+    char *dir;
+
+    dir = get_wd();
+    miniprintf((rep == 0 ? PROMPT : PROMPT_), "MINISHELL", dir);
+	if (dir != NULL)
+        free(dir);
+}
+
+void	sig_handler(int sig)
+{
+	rep = 130;
+	if (sig == SIGINT)
+	{
+		ft_putstr("\n");
+		ft_prompt();
+	}
+	if (sig == SIGQUIT) //ctrl-/
+		miniprintf("BONJOUR");
+}
+
+/*
+**	ctrl-D : ecriture  -> bloque : rien ne se passe
+**  ctrl-D : prog      -> ne fait rien
+**  ctrl-D : rien      -> exit bash (EOF->sur entree standard)
+**  ctrl-C : ecriture  -> retour a la ligne (et ^C)
+**  ctrl-C : prog      -> quitte le programme
+**  ctrl-C : rien      -> retour a la ligne (et ^C)
+**  ctrl-\ : ecriture  -> ?
+**  ctrl-\ : prog      -> "Quitter (core dumped)"
+**  ctrl-\ : rien      -> ?
+*/
+
+
+
+int		ft_isulsign(char caract)
+{
+	if ((caract == '<' || caract == '>'))
+		return (1);
+	return (0);
+}
+
+int		ft_checkread(char *read)
+{
+	size_t	i;
+	int		s;
+	char	prev;
+	char 	quote;
+
+	s = 0;
+	i = 0;
+	while (read[i])
+	{
+		quote_inside(&quote, read[i], prev);
+		while (!quote && read[i] == '<' || read[i] == '>')
+		{
+			s++;
+			i++;
+		}
+		if (s > 2)
+		{
+			miniprintf("ERROR : %c\n", read[i]);
+			free(read);
+			return (0);
+		}
+		else if (s > 0) 
+		{
+			while (ft_isspace(read[i]))
+				i++;
+			if (ft_isulsign(read[i]) || read[i] == 0)
+			{
+				if (read[i] != 0)
+					miniprintf("bash: erreur de syntaxe près du symbole inattendu « %c »\n", read[i]);
+				else
+					miniprintf("bash: erreur de syntaxe près du symbole inattendu « newline »\n");
+				free(read);
+				return (0);
+			} 
+			else
+				s = 0;
+		}	
+		prev = read[i];
+		i++;
+	}
+	return (1);
+}
+
+void	parse_read(char *read, t_list **env)
+{
+	t_list *line;
+	t_list *pipe;
+	t_list *start;
+
+	line = ft_lst_split(read, ";", 1);
+	start = line;
+	while (line)
+	{
+		pipe = ft_lst_split(line->content, "|", 1);
+		do_pipe(pipe, ft_lstsize(pipe), &rep, env);
+		ft_lstclear(&pipe, *free);
+		line = line->next;
+	}
+	ft_lstclear(&start, *free);
+	free(read);
+}
+
+int		main(int ac, char **av, char **env)
+{
+	t_list	*lst_env;
+	char	*read;
+	char *tmp;
+
+	rep = 0;
+	signal(SIGINT, sig_handler);
+	signal(SIGQUIT, sig_handler);
+	if (ac > 0)
+	{
+		lst_env = ft_ato_lst(env);
+		ft_prompt();
+		while (get_next_line(0, &read))
+		{
+			if (ft_checkread(read))
+				parse_read(read, &lst_env);
+			ft_prompt();
+		}
+		ft_lstclear(&lst_env, *free);
+		free(read);
+		miniprintf("exit\n");
+	}
+}
